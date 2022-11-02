413adf25f0141f6103f71570883ca4c8f4a5dfa8
Nabil <nkoney14@outlook.com>
2022-11-02 04:14:20 -0400

    Organizing server routes more

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,20 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const apiRouter = express.Router();
 
 // Middleware
-app.use(cors);
+app.use(cors); // mitigates cors errors
+app.use(express.json()); // allows us to parse json (built on body-parser)
 
-//Routes
-app.get('/', (req, res) => {
+// Routes for internal API
+apiRouter.get('/', (req, res) => {
   // TODO: Write response for what we need
 });
-
 //TODO: Write internal API routues for specific queries to the APIs EX: a route that returns the top 5 polling locations as the response
+
+// Use different routers for different routes
+app.use('/api', apiRouter); // use the apiRouter for all routes starting with /api
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Starting server on port ${process.env.PORT}`);
