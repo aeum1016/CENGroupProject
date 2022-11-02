b39963896ab1058eea7b4dfa496c0782e180756e
Nabil <nkoney14@outlook.com>
2022-11-02 14:55:07 -0400

    Trying to get some sample code for the server written

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+require('dotenv').config(); // configure the environment by adding varibales from the .env file
+
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+const axios = require('axios');
 
 const app = express();
 const apiRouter = express.Router();
@@ -9,15 +11,47 @@ const apiRouter = express.Router();
 app.use(cors); // mitigates cors errors
 app.use(express.json()); // allows us to parse json (built on body-parser)
 
+// Define constants to be used in server implementation
+const PORT = process.env.PORT;
+const CIVIC_INFO_BASE_URL = "https://www.googleapis.com/civicinfo/v2";
+const CIVIC_INFO_API_KEY = process.env.CIVIC_INFO_API_KEY;
+
 // Routes for internal API
-apiRouter.get('/', (req, res) => {
-  // TODO: Write response for what we need
+// creates a configuration object to be used in axios requests to the google civic information api
+const getCivicInfoConfig = (url, address = null) => {
+	let config = {
+		url: url,
+		method: 'get',
+		baseURL: CIVIC_INFO_BASE_URL,
+		params: {
+			key: CIVIC_INFO_API_KEY
+		}
+	};
+
+	if(address) {
+		config.params.address = address;
+	}
+
+	return(config);
+};
+
+apiRouter.get('/voterinfo', (req, res) => {
+  // const address = req.params.address;
+	axios(getCivicInfoConfig('/voterinfo', '607 Carpenter Way Auburn, Alabama(AL)'))
+		.then((axiosResponse) => {
+			const axiosData = axiosResponse.data;
+			res.send(axiosData);
+		})
+		.catch((err) => {
+			res.status(500).send(err);
+		});
 });
-//TODO: Write internal API routues for specific queries to the APIs EX: a route that returns the top 5 polling locations as the response
+
+//TODO: Write other internal API routues for specific queries to the APIs EX: a route that returns the top 5 polling locations as the response
 
 // Use different routers for different routes
 app.use('/api', apiRouter); // use the apiRouter for all routes starting with /api
 
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Starting server on port ${process.env.PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`Starting server on port ${PORT}`);
 })
