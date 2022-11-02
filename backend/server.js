99bd96ad5a75cec6c2f5d3af08a1dfb44300a47e
Nabil <nkoney14@outlook.com>
2022-11-02 02:14:21 -0400

    Reorganized in frontend and backend, added server stub code, created other supporting structure

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  // TODO: Write response for what we need
+});
+
+//TODO: Write internal API routues for specific queries to the APIs EX: a route that returns the top 5 polling locations as the response
