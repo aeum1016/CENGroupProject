592ed9b16efa217652cdf128ae2b9bfd096e95e3
Nabil <nkoney14@outlook.com>
2022-11-03 16:10:52 -0400

    Adding todos for the API routes

--- backend/server.js
+++ backend/server.js
@@ -44,14 +44,28 @@ apiRouter.get('/voterinfo/:address', (req, res) => {
 	axios(config)
 		.then((axiosResponse) => {
 			const axiosData = axiosResponse.data; // take the axios response as a function parameter and get the data
+
+			// TODO: Parse response and construct a new object with the properties we want 
+
 			res.send(axiosData); // send the data from axios as a response
 		})
 		.catch((err) => {
-			res.status(500).send(err); // send an HTTP error code along with the error if something fails
+			res.status(500)
+			res.send(err); // send an HTTP error code along with the error if something fails
 		});
 });
 
-//TODO: Write other internal API routues for specific queries to the APIs EX: a route that returns the top 5 polling locations as the response
+// TODO: Write other internal API routues for specific queries to the APIs EX: a route that returns the top 5 polling locations as the response
+
+// TODO: '/electionname/:address' - Get the election name
+
+// TODO: '/contests/:address' - Get the contests and respond in the form [ballotTitle : {office, position, referrendumTitle, referrendumUrl,[candidateName : {party, phone, email}]}, ...]
+
+// TODO: '/votinginformation/:address' - Get everything but the source from pollingLocations and early voting sites in the form [locationName : {address, [dropOffLocations], startDate, endDate}]
+
+// TODO: '/helpfulurls/:address' - {electionInfo, electionRegistration, electionRegistrationConfirmation, absenteeVoting, votingLocationFinder, ballotInfo}
+
+// TODO: '/representatives/:address' - {...}
 
 // Use different router objects for different routes
 app.use('/api', apiRouter); // use the apiRouter for all routes starting with /api
