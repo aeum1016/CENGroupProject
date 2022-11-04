986a7aafb3d3e28510ca87a0f06518aa717815f4
anniemichel <anne.michel@ufl.edu>
2022-11-03 22:46:07 -0400

    Remove changes from frontend Information Page

--- frontend/src/components/pages/InformationPage.js
+++ frontend/src/components/pages/InformationPage.js
@@ -2,27 +2,8 @@ import axios from 'axios';
 import React from 'react'
 
 function InformationPage() {
-  const address = '12605 Biscayne Bay Drive North Miami, FL'
-
-  const voterInfo = (e) => {
-    e.preventDefault();
-    const obj = {
-      address: {address}
-    }
-    axios.get('api/voterinfo/', {obj})
-    .then((response) => {
-      var result = response.data;
-      console.log(result);
-    }, (error) => {
-      console.log(error);
-    })
-  };
-
   return (
-    <div>
-      InformationPage
-      <button onClick={voterInfo}/>
-      </div>
+    <div>InformationPage</div>
   )
 }
 
