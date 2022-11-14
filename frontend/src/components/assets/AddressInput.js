11818129ccb77b1375e89acc983df038a0a437ee
Nabil <nkoney14@outlook.com>
2022-11-13 19:45:16 -0500

    Basic error checking and visual state change based on address input

--- frontend/src/components/assets/AddressInput.js
+++ frontend/src/components/assets/AddressInput.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import {
 	Stack,
 	TextField,
@@ -8,54 +8,122 @@ import {
 	Divider,
 } from '@mui/material';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import EditIcon from '@mui/icons-material/Edit';
 
 import states from '../../data/states'; // TODO: consider replacing with npm package such as react-select-us-states
 
 function AddressInput() {
 	// state for the different address components
-	const [streetAddress, setStreetAddress] = useState(null);
+	const [streetAddress, setStreetAddress] = useState(
+		localStorage['streetAddress']
+	);
 	const [streetAddressError, setStreetAddressError] = useState(false);
 
-	const [city, setCity] = useState(null);
+	const [city, setCity] = useState(localStorage['city']);
 	const [cityError, setCityError] = useState(false);
 
-	const [state, setState] = useState(null); // don't need error checking here because it's a dropdown
+	const [state, setState] = useState(localStorage['state']);
 	const [stateError, setStateError] = useState(false);
+
+	// state for overall adress component
+	const [address, setAddress] = useState(localStorage['address']);
+
+	// state for making the form disabled
+	const [isDisabled, setIsDisabled] = useState(false);
+
+	useEffect(() => {
+		// clearAddress(); // uncomment this to clear the localStorage when this component is rendered
+		if (address) {
+			setIsDisabled(!isDisabled);
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage['streetAddress'] = streetAddress;
+		localStorage['city'] = city;
+		localStorage['state'] = state;
+		localStorage['address'] = address;
+		console.log('New address set');
+	}, [address]);
+
+	// helper function to wipe the application state and local storage
+	const clearAddress = () => {
+		localStorage['streetAddress'] = '';
+		setStreetAddress('');
+
+		localStorage['city'] = '';
+		setCity('');
+
+		localStorage['state'] = '';
+		setState('');
+
+		localStorage['address'] = '';
+		setState('');
+	};
 
 	// functions to change the address
 	const handleStreetAddressChange = (event) => {
 		const newValue = event.target.value;
 		// TODO: check value against regex to set error value
-		setStreetAddressError(false);
+		if (!newValue || newValue == '') {
+			setStreetAddressError(true);
+		} else {
+			setStreetAddressError(false);
+		}
 		setStreetAddress(newValue);
 	};
 	const handleCityChange = (event) => {
 		const newValue = event.target.value;
 		// TODO: check value against regex to set error value
-		setCityError(false);
+		if (!newValue || newValue == '') {
+			setCityError(true);
+		} else {
+			setCityError(false);
+		}
 		setCity(newValue);
 	};
 	const handleStateChange = (event) => {
+		const newValue = event.target.value;
 		// TODO: check value against regex to set error value
-		setStateError(false);
-		setState(event.target.value);
+		if (!newValue || newValue == '') {
+			setStateError(true);
+		} else {
+			setStateError(false);
+		}
+		setState(newValue);
 	};
 
 	// store address in local storage
-	const submitAddress = () => {
-		const address = `${streetAddress} ${city}, ${state}`;
-		if (!streetAddress) {
+	const storeAddress = () => {
+		let error = false;
+		// if the error state has not been set yet in the onChange handlers handle that here
+		if (!streetAddress || streetAddress == '') {
 			setStreetAddressError(true);
+			error = true;
 		}
-		if (!city) {
+		if (!city || city == '') {
 			setCityError(true);
+			error = true;
 		}
-		if (!state) {
+		if (!state || state == '') {
 			setStateError(true);
+			error = true;
 		}
-
-		if (!(streetAddressError || cityError || stateError)) {
-			localStorage['address'] = address;
+		if (!error) {
+			const newAddress = `${streetAddress} ${city}, ${state}`;
+			setAddress(newAddress);
+			setIsDisabled(!isDisabled);
+		}
+	};
+	// put the address form into input mode
+	const editAddress = () => {
+		setIsDisabled(!isDisabled);
+	};
+	const onClick = () => {
+		if (isDisabled) {
+			editAddress();
+		} else {
+			storeAddress();
 		}
 	};
 
@@ -68,6 +136,7 @@ function AddressInput() {
 		>
 			<TextField
 				id='street-address'
+				value={streetAddress}
 				label='Street Address'
 				helperText={streetAddressError ? 'Ex: 607 Carpenter Way' : ' '}
 				onChange={handleStreetAddressChange}
@@ -75,9 +144,11 @@ function AddressInput() {
 				size={'small'}
 				fullWidth
 				required
+				disabled={isDisabled}
 			/>
 			<TextField
 				id='city'
+				value={city}
 				label='City'
 				helperText={cityError ? 'Ex: Auburn' : ' '}
 				onChange={handleCityChange}
@@ -85,6 +156,7 @@ function AddressInput() {
 				size={'small'}
 				fullWidth
 				required
+				disabled={isDisabled}
 			/>
 			<TextField
 				select
@@ -97,13 +169,18 @@ function AddressInput() {
 				size={'small'}
 				fullWidth
 				required
+				disabled={isDisabled}
 			>
 				{states.map((state) => {
-					return <MenuItem value={state}>{state}</MenuItem>;
+					return (
+						<MenuItem key={state} value={state}>
+							{state}
+						</MenuItem>
+					);
 				})}
 			</TextField>
-			<IconButton size='large' onClick={submitAddress}>
-				<SearchOutlinedIcon />
+			<IconButton size='large' onClick={onClick}>
+				{isDisabled ? <EditIcon /> : <SearchOutlinedIcon />}
 			</IconButton>
 		</Stack>
 	);
