61e6cc584787ac1f739dfd1ddf85a1b76adaf8be
Nabil <nkoney14@outlook.com>
2022-12-03 16:44:21 -0500

    More code cleanup

--- frontend/src/components/assets/PollingLocationsCard.js
+++ frontend/src/components/assets/PollingLocationsCard.js
@@ -12,28 +12,6 @@ import DashboardCard from './DashboardCard';
 import MapIcon from '@mui/icons-material/Map';
 import { Stack } from '@mui/system';
 
-const capitalize = (str) => {
-	let newStr = str.toLowerCase();
-	return newStr.charAt(0).toUpperCase() + newStr.slice(1);
-};
-const titleCase = (str) => {
-	console.log(str);
-	let newStr = str.toLowerCase();
-	let newTokenizedStr = newStr.split(' ');
-
-	if (newTokenizedStr.length < 1) {
-		return capitalize(newTokenizedStr[0]);
-	}
-
-	newStr = newTokenizedStr
-		.map((element) => {
-			return capitalize(element);
-		})
-		.join(' ');
-
-	return newStr;
-};
-
 const LocationIconButton = ({ line1, city, state, zipCode }) => {
 	let URL = `https://www.google.com/maps/place/${line1}, ${city}, ${state} ${zipCode}/`;
 
@@ -49,6 +27,29 @@ const LocationCard = ({ location }) => {
 	const pollingHours = location['pollingHours'];
 	const addressObject = location['address'];
 
+	const capitalize = (str) => {
+		let newStr = str.toLowerCase();
+		return newStr.charAt(0).toUpperCase() + newStr.slice(1);
+	};
+
+	const titleCase = (str) => {
+		console.log(str);
+		let newStr = str.toLowerCase();
+		let newTokenizedStr = newStr.split(' ');
+
+		if (newTokenizedStr.length < 1) {
+			return capitalize(newTokenizedStr[0]);
+		}
+
+		newStr = newTokenizedStr
+			.map((element) => {
+				return capitalize(element);
+			})
+			.join(' ');
+
+		return newStr;
+	};
+
 	// TODO: Polling hours for CA includes this information so leaving it out until needed
 	// const startDate = location['startDate'];
 	// const endDate = location['endDate'];
@@ -57,33 +58,30 @@ const LocationCard = ({ location }) => {
 	const line1 = titleCase(addressObject['line1']);
 	const city = titleCase(addressObject['city']);
 	const state = titleCase(addressObject['state']);
-	const address = `${line1} ${city}, ${state}`;
 	const zipCode = addressObject['zip'];
 
 	return (
-		<Box>
-			<Container>
-				<Box
-					display={'flex'}
-					justifyContent={'space-between'}
-					alignItems={'center'}
-				>
-					<Stack p={1} spacing={1}>
-						<Typography>{locationName}</Typography>
-						<Typography variant='body2'>{pollingHours}</Typography>
-					</Stack>
-					<LocationIconButton
-						line1={line1}
-						city={city}
-						state={state}
-						zipCode={zipCode}
-					/>
-				</Box>
-				<Box pt={1}>
-					<Divider />
-				</Box>
-			</Container>
-		</Box>
+		<Container>
+			<Box
+				display={'flex'}
+				justifyContent={'space-between'}
+				alignItems={'center'}
+			>
+				<Stack p={1} spacing={1}>
+					<Typography>{locationName}</Typography>
+					<Typography variant='body2'>{pollingHours}</Typography>
+				</Stack>
+				<LocationIconButton
+					line1={line1}
+					city={city}
+					state={state}
+					zipCode={zipCode}
+				/>
+			</Box>
+			<Box pt={1}>
+				<Divider />
+			</Box>
+		</Container>
 	);
 };
 const LocationCards = ({ locations }) => {
